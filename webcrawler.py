376b0e76dda6841fbe7e2afb4e7fc4ee2973c78e
Matheus Catossi <matheuscatossi@gmail.com>
2017-03-14 17:38:42 +0000

    Salvando dados nos arquivos

--- webcrawler.py
+++ webcrawler.py
@@ -13,27 +13,33 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 arquivo1 = csv.writer(open("base_teste.csv", "w"))
-arquivo2 = csv.writer(open("base_teste_json.json", "w"))
+arquivo2 = open("base_teste_json.json", "w")
 
 row = []
 
-statuses = tweepy.Cursor(api.search, q="#cblol", since="2017-03-01", until="2017-03-07", lang="pt").items()
-
+statuses = tweepy.Cursor(api.search, q="#uranet", since="2017-03-10", until="2017-03-15").items()
+#, lang="pt"
 
 while True:
-    try:
-        status = statuses.next()
-        print (status)
-        exit()
-        
-    except tweepy.TweepError as e:
-        
-        print (e.api_code)
-        
-        print("Aguarde 15 minutes")
-        time.sleep(60*15)
-        continue
-    except StopIteration:
-        print ("Acabou")
-        break
+  try:
+    status = statuses.next()
+    
+    print(status)
+    
+    row = str(status.user.screen_name), str(status.created_at), str(status.text), status.geo
+    arquivo1.writerow(row)
+    
+    arquivo2.write(str(status))
+    arquivo2.write("\n")
+    
+    
+    exit()
+  except tweepy.TweepError as e:
+    print (e.api_code)
+    print("Aguarde 15 minutes")
+    time.sleep(60*15)
+    continue
+  except StopIteration:
+    print ("Acabou")
+    break
         
